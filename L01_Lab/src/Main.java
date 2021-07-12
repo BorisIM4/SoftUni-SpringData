d851a97a7b075f9adda73393931a7f99b5227fb9
JRB <jrb@abv.bg>
2021-07-12 21:50:17 +0300

    Add Documentation to L01_Lab

--- L01_Lab/src/Main.java
+++ L01_Lab/src/Main.java
@@ -3,19 +3,37 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
+        //Създавеме връзка към базата
+        //jdbc -> Това е конективитито.Казваме, че ще изпозлваме java database connectivity Driver
+        //mysql -> Това е драйвърния протокол. Казваме че с java database connectivity Driver ще се вържем към вида база mysql
+        ////localhost:3306 - > Тук подаваме адреса на базата към която ще се вържем.(212.100.200.5 - това също е валиден адрес)
+        // soft_uni -> Това е името на БАзата към която искаме да се вържем.
+        // "root", "" -> Тук подаваме потребителлското име и паролата за достъп до базата.
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/soft_uni", "root", "");
 
-        Statement statement = connection.createStatement();
+//        //statement
+          //Създаваме стейтмънт, койото да води към базата
+//        Statement statement = connection.createStatement();
+//
+          //Стейтмънта подава заявка към базата и Връща резултат от данните в Сет.
+//        ResultSet resultSet = statement.executeQuery("SELECT * FROM employees WHERE salary > 50000 ");
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM employees WHERE salary > 50000 ");
+        //Създавам stmt който праща заявка към базата
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employees WHERE salary > ?");
 
+        //Подаваме параметър на стейтмънта. Така се предпаваме от SQL Injection.
+        preparedStatement.setString(1, "50000");
 
-        while (resultSet.next()) {
-            String id = resultSet.getString(1);
-            String name = resultSet.getString(2);
-            String salary = resultSet.getString(7);
+        //Стейтмънмта връща резултат в Сет, койтоо може да обходим.
+        ResultSet rs = preparedStatement.executeQuery();
+
+        while (rs.next()) {
+            String id = rs.getString(1);
+            String name = rs.getString(2);
+            String salary = rs.getString(7);
             System.out.println("id: " + id + " Name: " + name + " salary: " + salary);
         }
+
 
     }
 }
