7505865d27aeabd1660b8c1c5f32ddb36f7df5d2
JRB <jrb@abv.bg>
2022-04-06 18:42:17 +0300

    Add L05 Lab tasks

---
+++ L05_Spring_Data_Intro/Lab/05-sping-data-intro-lab/src/main/java/com/example/spingdataintrolab/SpingDataIntroLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingdataintrolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingDataIntroLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingDataIntroLabApplication.class, args);
+    }
+
+}
