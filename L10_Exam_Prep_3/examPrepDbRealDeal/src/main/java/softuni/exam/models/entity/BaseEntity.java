0fcb0456e28daf61403f26c12bdf08767d687135
JRB <jrb@abv.bg>
2022-12-19 21:05:58 +0200

    add l10 exam prep v2

---
+++ L10_Exam_Prep_3/examPrepDbRealDeal/src/main/java/softuni/exam/models/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package softuni.exam.models.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GenerationType;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ L10_Exam_Prep_3/examPrepDbRealDeal/src/main/java/softuni/exam/models/entity/Car.java
@@ -0,0 +1,53 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "cars")
+public class Car extends BaseEntity{
+
+    private String make;
+    private String model;
+    private Integer kilometers;
+    private LocalDate registeredOn ;
+
+    public Car(){
+    }
+
+    @Column
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    @Column
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Column
+    public Integer getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(Integer kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    @Column(name = "registered_on")
+    public LocalDate getRegisteredOn() {
+        return registeredOn;
+    }
+
+    public void setRegisteredOn(LocalDate registeredOn) {
+        this.registeredOn = registeredOn;
+    }
+}
